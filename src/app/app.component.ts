59d302f473650ae34aba87cef5773ceb08019408
Jay Patel <jayptl96@gmail.com>
2018-04-19 00:49:21 -0400

    Login/Register/Forget

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { HomePage } from '../pages/home/home';
+import firebase from 'firebase';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any;
+  public zone:NgZone;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    this.zone = new NgZone({});
+    const config = {
+      apiKey: "AIzaSyBGLE19gJq1oawff7u-tv09LlRXz3__fmE",
+      authDomain: "whatarticle.firebaseapp.com",
+      databaseURL: "https://whatarticle.firebaseio.com",
+      projectId: "whatarticle",
+      storageBucket: "whatarticle.appspot.com",
+      messagingSenderId: "28968695656"
+    };
+    firebase.initializeApp(config);
+
+    firebase.auth().onAuthStateChanged( user => {
+      this.zone.run( () => {
+        if (!user) { 
+          this.rootPage = 'LoginPage';
+        } else {
+          this.rootPage = HomePage;
+        }
+      });     
+    });
+
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
 }
-
